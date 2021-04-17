7e4e00dec4bf5205e210ebfc6086941237af9732
Arekjaar <arekjaar4android@gmail.com>
2021-04-17 15:30:36 +0200

    delete fake product array

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,6 @@ void main() {
   runApp(MyApp());
 }
 
-//var products = [{"name": "Zapatillas", "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum", "price":"20.00", "image":"https://st2.depositphotos.com/3746151/5928/v/950/depositphotos_59287971-stock-illustration-pair-of-red-sneakers.jpg"},
-//              {"name": "producto_2", "description":"Descripcion producto 2", "price":"30.00", "image":"https://st2.depositphotos.com/3746151/5928/v/950/depositphotos_59287971-stock-illustration-pair-of-red-sneakers.jpg"}];
-
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
